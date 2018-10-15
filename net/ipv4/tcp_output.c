16d9c775c64f53a842255c0edc4813aa94be2c4a
Eric Dumazet <edumazet@google.com>
2018-10-15 09:37:52 -0700

    BACKPORT: tcp: do not change tcp_wstamp_ns in tcp_mstamp_refresh
    In EDT design, I made the mistake of using tcp_wstamp_ns
    to store the last tcp_clock_ns() sample and to store the
    pacing virtual timer.
    
    This causes major regressions at high speed flows.
    
    Introduce tcp_clock_cache to store last tcp_clock_ns().
    This is needed because some arches have slow high-resolution
    kernel time service.
    
    tcp_wstamp_ns is only updated when a packet is sent.
    
    Note that we can remove tcp_mstamp in the future since
    tcp_mstamp is essentially tcp_clock_cache/1000, so the
    apparent socket size increase is temporary.
    
    Fixes: 9799ccb0e984 ("tcp: add tcp_wstamp_ns socket field")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Soheil Hassas Yeganeh <soheil@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    (cherry picked from commit ae02e94b2c6f7a684155fbf90c29d2e4e465d287)
    (cherry picked from commit ad3791f934fcc010cbfc8296def6750668b6163a)
    (cherry picked from commit ebfb1aa51b350bcf2069eba59c8654663ca572c8)
    (cherry picked from commit b2ea7a6bcc42bda5ccc3f66556efeaca6ab5e74f)
    (cherry picked from commit ee8de42d9abe9d816ef9ee12edf86935094fe028)
    (cherry picked from commit a5e0003efb37fcdb2fc6833bd0b701154a53735d)
    (cherry picked from commit eb66b875d4c12ce534bc9ec2551d40b34bf9f3cd)
    (cherry picked from commit bd0ae476a1a16d1a3da42e7c5d74c00f95ddaf0e)
    (cherry picked from commit e294460f24af8e7ad5b5757ee0785c613fd3e3fa)
    (cherry picked from commit 37269ba368aa9eea4e4e77a6301fc85bddf747d0)
    Signed-off-by: Pajdek <pajdek@gmail.com>
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -1216,6 +1216,9 @@ static int __tcp_transmit_skb(struct sock *sk, struct sk_buff *skb,
 
 	skb->skb_mstamp_ns = tp->tcp_wstamp_ns;
 
+	/* TODO: might take care of jitter here */
+	tp->tcp_wstamp_ns = max(tp->tcp_wstamp_ns, tp->tcp_clock_cache);
+
 	inet = inet_sk(sk);
 	tcb = TCP_SKB_CB(skb);
 	memset(&opts, 0, sizeof(opts));

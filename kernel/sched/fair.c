3146842cc47d24cf1fc7501f0f3293c0bd32a995
Valentin Schneider <valentin.schneider@arm.com>
2019-01-17 15:34:07 +0000

    sched/fair: Simplify nohz_balancer_kick()
    Calling 'nohz_balance_exit_idle(rq)' will always clear 'rq->cpu' from
    'nohz.idle_cpus_mask' if it is set. Since it is called at the top of
    'nohz_balancer_kick()', 'rq->cpu' will never be set in
    'nohz.idle_cpus_mask' if it is accessed in the rest of the function.
    
    Combine the 'sched_domain_span()' with 'nohz.idle_cpus_mask' and drop the
    '(i == cpu)' check since 'rq->cpu' will never be iterated over.
    
    While at it, clean up a condition alignment.
    
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Dietmar.Eggemann@arm.com
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: morten.rasmussen@arm.com
    Cc: vincent.guittot@linaro.org
    Link: https://lkml.kernel.org/r/20190117153411.2390-2-valentin.schneider@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    [ @ Tashar02: Adapt for commit 901863ad0 ("sched: Don't update idle_cpus_mask during core isolation") on k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10595,7 +10595,7 @@ static void nohz_balancer_kick(struct rq *rq)
 	sd = rcu_dereference(rq->sd);
 	if (sd) {
 		if ((rq->cfs.h_nr_running >= 1) &&
-				check_cpu_capacity(rq, sd)) {
+		    check_cpu_capacity(rq, sd)) {
 			flags = NOHZ_KICK_MASK;
 			goto unlock;
 		}
@@ -10603,11 +10603,7 @@ static void nohz_balancer_kick(struct rq *rq)
 
 	sd = rcu_dereference(per_cpu(sd_asym_packing, cpu));
 	if (sd) {
-		for_each_cpu(i, sched_domain_span(sd)) {
-			if (i == cpu ||
-			    !cpumask_test_cpu(i, nohz.idle_cpus_mask))
-				continue;
-
+		for_each_cpu_and(i, sched_domain_span(sd), &cpumask) {
 			if (sched_asym_prefer(i, cpu)) {
 				flags = NOHZ_KICK_MASK;
 				goto unlock;

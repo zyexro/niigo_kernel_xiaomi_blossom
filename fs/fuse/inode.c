005711d8dd5c0319d1996d8bb340a1ba44ac9815
Stefan Hajnoczi <stefanha@redhat.com>
2020-08-19 18:19:49 -0400

    UPSTREAM: virtiofs: implement FUSE_INIT map_alignment field
    The device communicates FUSE_SETUPMAPPING/FUSE_REMOVMAPPING alignment
    constraints via the FUST_INIT map_alignment field.  Parse this field and
    ensure our DAX mappings meet the alignment constraints.
    
    We don't actually align anything differently since our mappings are
    already 2MB aligned.  Just check the value when the connection is
    established.  If it becomes necessary to honor arbitrary alignments in
    the future we'll have to adjust how mappings are sized.
    
    The upshot of this commit is that we can be confident that mappings will
    work even when emulating x86 on Power and similar combinations where the
    host page sizes are different.
    
    Change-Id: I8c7b002aa413dbdae43dc159d82cba672e671351
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -924,9 +924,10 @@ static void process_init_reply(struct fuse_conn *fc, struct fuse_args *args,
 {
 	struct fuse_init_args *ia = container_of(args, typeof(*ia), args);
 	struct fuse_init_out *arg = &ia->out;
+	bool ok = true;
 
 	if (error || arg->major != FUSE_KERNEL_VERSION)
-		fc->conn_error = 1;
+		ok = false;
 	else {
 		unsigned long ra_pages;
 
@@ -989,6 +990,11 @@ static void process_init_reply(struct fuse_conn *fc, struct fuse_args *args,
 					min_t(unsigned int, FUSE_MAX_MAX_PAGES,
 					max_t(unsigned int, arg->max_pages, 1));
 			}
+			if (IS_ENABLED(CONFIG_FUSE_DAX) &&
+			    arg->flags & FUSE_MAP_ALIGNMENT &&
+			    !fuse_dax_check_alignment(fc, arg->map_alignment)) {
+				ok = false;
+			}
 			if (arg->flags & FUSE_PASSTHROUGH) {
 				fc->passthrough = 1;
 				/* Prevent further stacking */
@@ -1009,6 +1015,11 @@ static void process_init_reply(struct fuse_conn *fc, struct fuse_args *args,
 		fc->conn_init = 1;
 	}
 	kfree(ia);
+
+	if (!ok) {
+		fc->conn_init = 0;
+		fc->conn_error = 1;
+	}
 
 	fuse_set_initialized(fc);
 	wake_up_all(&fc->blocked_waitq);
@@ -1034,6 +1045,10 @@ void fuse_send_init(struct fuse_conn *fc)
 		FUSE_ABORT_ERROR | FUSE_MAX_PAGES | FUSE_CACHE_SYMLINKS |
 		FUSE_NO_OPENDIR_SUPPORT | FUSE_EXPLICIT_INVAL_DATA |
 		FUSE_PASSTHROUGH;
+#ifdef CONFIG_FUSE_DAX
+	if (fc->dax)
+		ia->in.flags |= FUSE_MAP_ALIGNMENT;
+#endif
 	ia->args.opcode = FUSE_INIT;
 	ia->args.in_numargs = 1;
 	ia->args.in_args[0].size = sizeof(ia->in);

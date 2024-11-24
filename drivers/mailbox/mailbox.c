f32e790f0cca6d51e1dc535b1b1bbc80d11ed40e
fukiame <fukiame@proton.me>
2024-11-24 22:27:06 +0700

    fixup! drivers: mailbox: merge derp
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/mailbox/mailbox.c
+++ drivers/mailbox/mailbox.c
@@ -103,11 +103,11 @@ static void msg_submit(struct mbox_chan *chan)
 		err = __msg_submit(chan);
 	} while (err == -EAGAIN);
 
+	/* kick start the timer immediately to avoid delays */
 	if (!err && (chan->txdone_method & TXDONE_BY_POLL)) {
-		/* kick start the timer immediately to avoid delays */
-		spin_lock_irqsave(&chan->mbox->poll_hrt_lock, flags);
-		hrtimer_start(&chan->mbox->poll_hrt, 0, HRTIMER_MODE_REL);
-		spin_unlock_irqrestore(&chan->mbox->poll_hrt_lock, flags);
+		/* but only if not already active */
+		if (!hrtimer_active(&chan->mbox->poll_hrt))
+			hrtimer_start(&chan->mbox->poll_hrt, 0, HRTIMER_MODE_REL);
 	}
 }
 

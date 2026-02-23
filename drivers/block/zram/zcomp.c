de338ac2c4d7884b89bd8c2e7f17fab6b25d5881
zyexro <zyexro@proton.me>
2026-02-23 22:24:36 +0100

    Revert "lib: drop lz4_oplus"
    This reverts commit e81d35b56d48288077727bd74e5a794ac4d42e91.

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -30,7 +30,9 @@ static const char * const backends[] = {
 #if IS_ENABLED(CONFIG_CRYPTO_LZ4K)
 	"lz4k",
 #endif
-
+#if IS_ENABLED(CONFIG_CRYPTO_LZ4K_OPLUS)
+	"lz4k_oplus",
+ #endif
 #if IS_ENABLED(CONFIG_CRYPTO_LZ4KD)
 	"lz4kd",
 #endif

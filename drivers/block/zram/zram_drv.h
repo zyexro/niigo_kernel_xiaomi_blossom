7c0d34ece4bcadc59c3be4936a9def3c45408e08
zyexro <zyexro@proton.me>
2026-04-04 15:47:22 +0100

    zram: restore ZRAM_HUGE
    Signed-off-by: zyexro <zyexro@proton.me>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -50,6 +50,7 @@ enum zram_pageflags {
 	ZRAM_SAME,	/* Page consists the same element */
 	ZRAM_WB,	/* page is stored on backing_device */
 	ZRAM_UNDER_WB,	/* page is under writeback */
+	ZRAM_HUGE,      /* Incompressible page */
 	ZRAM_IDLE,	/* not accessed page since last idle marking */
 	ZRAM_INCOMPRESSIBLE, /* none of the algorithms could compress it */
 

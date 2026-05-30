b278dce1ae564ad35160ebfb12630ecce1232f4d
ris <rissu.ntk@gmail.com>
2026-05-30 18:29:18 +0700

    simple_lmk: convert si_meminfo into totalram_pages
    * Also convert it to use megabytes
    
    Change-Id: I218a0ccc7df91a2bce33aa21046a91af3f624a78
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -485,7 +485,7 @@ static int simple_lmk_init_set(const char *val, const struct kernel_param *kp)
 {
 	static atomic_t init_done = ATOMIC_INIT(0);
 	struct task_struct *thread;
-	struct sysinfo i;
+	unsigned long total_mb = 0;
 
 	if (!atomic_cmpxchg(&init_done, 0, 1)) {
 		thread = kthread_run(simple_lmk_reaper_thread, NULL,
@@ -497,17 +497,19 @@ static int simple_lmk_init_set(const char *val, const struct kernel_param *kp)
 		BUG_ON(vmpressure_notifier_register(&vmpressure_notif));
 	}
 
-	si_meminfo(&i);
-	if (i.totalram << (PAGE_SHIFT-10) > 4096ull * 1024) {
-	  // from - phone-xhdpi-6144-dalvik-heap.mk
-	  slmk_minfree = 125;
-	  slmk_timeout = 160;
+	total_mb = totalram_pages >> (20 - PAGE_SHIFT);
+	if (total_mb > 3072) {
+		// 4GB+ variant
+		slmk_minfree = 155;
+		slmk_timeout = 145;
 	} else {
-	  // from - phone-xhdpi-4096-dalvik-heap.mk
-	  slmk_minfree = 155;
-	  slmk_timeout = 145;
+		// 3GB or lower
+		slmk_minfree = 256;
+		slmk_timeout = 200;
 	}
 
+	pr_info_once("Detected %lu megabytes of device RAM, setting minfree to %hu with timeout of %hu\n",
+		total_mb, slmk_minfree, slmk_timeout);
 	return 0;
 }
 

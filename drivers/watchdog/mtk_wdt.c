4b0d8e18fa839a164510f32137c9d460d8acb0b3
danya2271 <dany2271@yandex.ru>
2026-06-25 22:15:42 +0500

    watchdog: mtk_wdt: preserve hardware state across suspend

--- drivers/watchdog/mtk_wdt.c
+++ drivers/watchdog/mtk_wdt.c
@@ -71,6 +71,7 @@ struct mtk_wdt_dev {
 	void __iomem *wdt_base;
 	spinlock_t lock; /* protects WDT_SWSYSRST reg */
 	struct reset_controller_dev rcdev;
+	bool suspend_wdt_running;
 };
 
 struct mtk_wdt_data {
@@ -353,7 +354,9 @@ static int mtk_wdt_probe(struct platform_device *pdev)
 static int mtk_wdt_suspend(struct device *dev)
 {
 	struct mtk_wdt_dev *mtk_wdt = dev_get_drvdata(dev);
-	if (watchdog_hw_running(&mtk_wdt->wdt_dev))
+	mtk_wdt->suspend_wdt_running =
+		watchdog_hw_running(&mtk_wdt->wdt_dev);
+	if (mtk_wdt->suspend_wdt_running)
 		mtk_wdt_stop(&mtk_wdt->wdt_dev);
 
 	return 0;
@@ -363,9 +366,10 @@ static int mtk_wdt_resume(struct device *dev)
 {
 	struct mtk_wdt_dev *mtk_wdt = dev_get_drvdata(dev);
 
-	if (watchdog_hw_running(&mtk_wdt->wdt_dev)) {
+	if (mtk_wdt->suspend_wdt_running) {
 		mtk_wdt_start(&mtk_wdt->wdt_dev);
 		mtk_wdt_ping(&mtk_wdt->wdt_dev);
+		mtk_wdt->suspend_wdt_running = false;
 	}
 
 	return 0;

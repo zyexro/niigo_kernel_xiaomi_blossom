ac7f6405e8a750e13abc09e7dc8fd6dd75436fb7
danya2271 <danya2271@yandex.ru>
2026-07-02 00:30:43 +0500

    cmdq: embed mdp pmqos task record

--- drivers/misc/mediatek/cmdq/v3/cmdq_helper_ext.h
+++ drivers/misc/mediatek/cmdq/v3/cmdq_helper_ext.h
@@ -9,6 +9,7 @@
 #include <linux/kernel.h>
 #include <linux/printk.h>
 #include <linux/soc/mediatek/mtk-cmdq.h>
+#include <linux/time.h>
 #include <linux/trace_events.h>
 
 #include "cmdq_def.h"
@@ -603,6 +604,12 @@ struct task_private {
 	bool ignore_timeout;	/* timeout is expected */
 };
 
+struct mdp_pmqos_record {
+	uint32_t mdp_throughput;
+	struct timeval submit_tm;
+	struct timeval end_tm;
+};
+
 enum cmdq_thread_dispatch {
 	CMDQ_THREAD_NOTSET = 0,
 	CMDQ_THREAD_STATIC,
@@ -644,7 +651,6 @@ struct cmdqRecStruct {
 	const char *sram_owner_name;
 	u32 sram_base;	/* Original PA address of SRAM buffer content */
 	void *node_private;
-	void *user_private;
 
 	struct cmdqSecDataStruct secData;	/* secure execution data */
 
@@ -711,6 +717,7 @@ struct cmdqRecStruct {
 	/* PMQoS information */
 	void *prop_addr;
 	u32 prop_size;
+	struct mdp_pmqos_record pmqos_record;
 
 	/* secure world */
 	struct iwcCmdqSecStatus_t *secStatus;

c423d64f0fa3f1f039e0ed01d1e88bb04b7760c5
danya2271 <danya2271@yandex.ru>
2026-07-11 16:26:17 +0500

    mediatek: wlan: remove production QA daemon

--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_qa_agent.c
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_qa_agent.c
@@ -9514,6 +9514,7 @@ static struct HQA_CMD_TABLE HQA_CMD_TABLES[] = {
  * \retval 0			On success.
  */
 /*----------------------------------------------------------------------------*/
+#if CFG_SUPPORT_QA_AGENT
 int HQA_CMDHandler(struct net_device *prNetDev,
 		   IN union iwreq_data *prIwReqData,
 		   struct HQA_CMD_FRAME *HqaCmdFrame)
@@ -9654,4 +9655,5 @@ ERROR1:
 ERROR0:
 	return i4Status;
 }
+#endif
 #endif
